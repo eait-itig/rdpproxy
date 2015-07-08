e40de7a998089934f1bc0ff46126b99fda0cb5c8
Alex Wilson <alex@uq.edu.au>
2015-07-08 13:19:36 +1000

    Restrict access to status update API using dns

--- apps/rdpproxy/src/http_api.erl
+++ apps/rdpproxy/src/http_api.erl
@@ -30,6 +30,8 @@
 -module(http_api).
 -export([start/0, peer_allowed/2]).
 
+-include_lib("kernel/include/inet.hrl").
+
 start() ->
     Port = rdpproxy:config([http_api, port], 8080),
     Dispatch = cowboy_router:compile([
@@ -47,7 +49,53 @@ start() ->
     cowboy:start_http(http, 20, [{port, Port}],
         [{env, [{dispatch, Dispatch}]}]).
 
-peer_allowed(Ip, PeerIp) ->
-    {A,B,_C,_D} = PeerIp,
-    [A1,B1,_C1,_D1] = [list_to_integer(X) || X <- string:tokens(binary_to_list(Ip), ".")],
-    (A =:= 10) andalso (A =:= A1) andalso (B =:= B1).
+rev_lookup(Ip) ->
+    case inet_res:gethostbyaddr(Ip) of
+        {ok, #hostent{h_name = Name}} when is_list(Name) ->
+            case inet_res:gethostbyname(Name) of
+                {ok, #hostent{h_addr_list = Addrs}} ->
+                    case lists:member(Ip, Addrs) of
+                        true -> {ok, Name};
+                        false -> {error, reverse_not_match}
+                    end;
+                _ -> {error, reverse_not_found}
+            end;
+        E -> {error, E}
+    end.
+
+is_rfc1918({A,B,C,D}) ->
+    (A =:= 10) orelse
+        ((A =:= 192) andalso (B =:= 168)) orelse
+        ((A =:= 172) andalso (B >= 16) andalso (B =< 31)).
+
+same_slash16({A,B,C,D}, {A1,B1,C1,D1}) ->
+    (A =:= A1) andalso (B =:= B1).
+
+peer_allowed(TargetIpStr, AgentIp) ->
+    TargetIp = list_to_tuple([list_to_integer(X) || X <-
+        string:tokens(binary_to_list(TargetIpStr), ".")]),
+    case rev_lookup(AgentIp) of
+        {ok, AgentName} ->
+            Suffix = rdpproxy:config([http_api, agent_dns_suffix]),
+            case lists:suffix(Suffix, AgentName) of
+                true ->
+                    DoDnsMatch = rdpproxy:config([http_api,
+                        check_agent_dns_matches_host], false),
+                    case DoDnsMatch of
+                        true ->
+                            [Prefix | _] = string:tokens(AgentName, "."),
+                            case rev_lookup(TargetIp) of
+                                {ok, TargetName} ->
+                                    lists:prefix(Prefix, TargetName) andalso
+                                        is_rfc1918(AgentIp) andalso
+                                        same_slash16(TargetIp, AgentIp);
+                                _ -> false
+                            end;
+                        false ->
+                            is_rfc1918(AgentIp) andalso
+                                same_slash16(TargetIp, AgentIp)
+                    end;
+                false -> false
+            end;
+        _ -> false
+    end.
